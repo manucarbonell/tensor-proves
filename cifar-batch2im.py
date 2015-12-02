94d70f112be5ec6e7d0bed79a8640806e55218a8
Manuel Carbonell Nuñez <manuelcarbonellnunez@MacBook-de-Manuel.local>
2015-12-02 16:37:39 +0100

    image index works

--- cifar-batch2im.py
+++ cifar-batch2im.py
@@ -2,7 +2,6 @@ import os
 import io
 import numpy as np
 import struct
-import greyscale
 from PIL import Image
 from array import array
 size=(32,32)
@@ -11,8 +10,8 @@ def save_image( npdata, outfilename ) :
     img = Image.fromarray( np.asarray( np.clip(npdata,0,255), dtype="uint8"), "RGB" )
     img.thumbnail(size,Image.ANTIALIAS)
     img.save(outfilename)
-
-MAX_BYTES=100000
+print "Introduce the number of images to read"
+MAX_BYTES=3073*(int(raw_input()))
 i=0
 f = open("data_batch_1.bin", "rb")
 im=np.zeros((32,32,3))
@@ -21,11 +20,10 @@ while i<MAX_BYTES:
     value = struct.unpack('B', byte)[0]
     if(i%3073==0):
         name=str(value)
-        print name
         byte = f.read(1)
         i=i+1
     value = struct.unpack('B', byte)[0]
-    im[((i-1)%1024)/32,(i-1)%32,((i-1)%3072)/1024]=value
+    im[((i-i/3073)%1024)/32,(i-i/3073)%32,((i-i/3073)%3072)/1024]=value
     i=i+1
     if(i%3073==0):
-        save_image(im,"pictures/"+name+".jpg")
+        save_image(im,"pictures/"+name+'_'+str(i)+".jpg")
